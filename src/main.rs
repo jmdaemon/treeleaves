cd9018e1aab7395fa5ab084ae87fd98d5ab58b9e
Joseph Diza <josephm.diza@gmail.com>
2022-11-26 00:18:09 -0800

    Add source field to image files database

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,33 @@ use rusqlite::{Connection, Result};
 struct ImageFile {
     id: i32,
     filename: String,
-    //tags: Vec<String>,
     tags: String,
     creation_date: String,
     last_modified: String,
     sha1: String,
     md5: String,
+    source: String,
 }
 
-fn main() -> Result<()> {
-    let conn = Connection::open_in_memory()?;
-
-    const SQL_CREATE_IMAGES_TABLE_TAGS_BLOG: &str = "\
-    CREATE TABLE images ( \
-        id    INTEGER PRIMARY KEY, \
-        filename TEXT NOT NULL, \
-        tags BLOB, \
-        creation_date TEXT NOT NULL, \
-        last_modified TEXT NOT NULL, \
-        sha1 TEXT NOT NULL, \
-        md5 TEXT NOT NULL,
-    )";
-
+fn create_images_db(conn: &Connection) -> Result<usize, rusqlite::Error> {
     const SQL_CREATE_IMAGES_TABLE_TAGS_STRING: &str = "
     CREATE TABLE images (
         id    INTEGER PRIMARY KEY,
         filename TEXT NOT NULL,
-        tags TEXT NOT NULL,
+        tags TEXT,
         creation_date TEXT NOT NULL,
         last_modified TEXT NOT NULL,
         sha1 TEXT NOT NULL,
-        md5 TEXT NOT NULL
+        md5 TEXT NOT NULL,
+        source BLOB
     )";
 
     conn.execute(
         SQL_CREATE_IMAGES_TABLE_TAGS_STRING, (), // empty list of parameters
-    )?;
+    )
+}
 
+fn test_image_db_insert(conn: &Connection) -> Result<usize, rusqlite::Error> {
     let test_image = ImageFile {
         id: 0,
         filename: "~/File.png".to_string(),
@@ -51,15 +41,23 @@ fn main() -> Result<()> {
         last_modified: "2022-11-24T00:00:00".to_string(),
         sha1: "b7fcec7f68cb90f18c39988f69c632d94e829d0c".to_string(),
         md5: "fd2f82b6722681764075fce841a31b6f".to_string(),
+        source: "https://ponerpics.org/images/1048610".to_string()
     };
 
     conn.execute(
-        "INSERT INTO images (filename, tags, creation_date, last_modified, sha1, md5) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+        "INSERT INTO images (filename, tags, creation_date, last_modified, sha1, md5, source) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
         (&test_image.filename, &test_image.tags, &test_image.creation_date,
-         &test_image.last_modified, &test_image.sha1, &test_image.md5),
-    )?;
+         &test_image.last_modified, &test_image.sha1, &test_image.md5, &test_image.source),
+    )
+}
 
-    let mut stmt = conn.prepare("SELECT id, filename, tags, creation_date, last_modified, sha1, md5 FROM images")?;
+fn main() -> Result<()> {
+    let conn = Connection::open_in_memory()?;
+
+    create_images_db(&conn)?;
+    test_image_db_insert(&conn)?;
+
+    let mut stmt = conn.prepare("SELECT id, filename, tags, creation_date, last_modified, sha1, md5, source FROM images")?;
     let image_iter = stmt.query_map([], |row| {
         Ok(ImageFile {
             id: row.get(0)?,
@@ -69,6 +67,7 @@ fn main() -> Result<()> {
             last_modified: row.get(4)?,
             sha1: row.get(5)?,
             md5: row.get(6)?,
+            source: row.get(7)?,
         })
     })?;
 
