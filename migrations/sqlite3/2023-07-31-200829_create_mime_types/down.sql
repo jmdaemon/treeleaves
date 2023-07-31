3c39b9bda2981c56e8cddb6923b72fb9835cd67b
Joseph Diza <josephm.diza@gmail.com>
2023-07-31 13:35:48 -0700

    Add mime_types table migration

---
+++ migrations/sqlite3/2023-07-31-200829_create_mime_types/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+
+drop TABLE mime_types
---
+++ migrations/sqlite3/2023-07-31-200829_create_mime_types/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+
+create TABLE mime_types (
+    id INTEGER PRIMARY KEY,
+    type VARCHAR NOT NULL
+)
