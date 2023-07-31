aff5c12c1863641e31f81e3e9c4673d65524576d
Joseph Diza <josephm.diza@gmail.com>
2023-07-31 00:34:33 -0700

    Split setup into migrations, setup

---
+++ migrations.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Make the migrations directories
+MIGRATIONS_ROOT_DIR="migrations"
+DATABASE_TYPE="sqlite3"
+
+migrations=(
+    "create_files"
+    "create_main"
+    "create_images"
+    "create_audio"
+    "create_videos"
+    )
+
+# Create the migrations root directory
+MIGRATIONS_DIR="$MIGRATIONS_ROOT_DIR/$DATABASE_TYPE"
+if [[ ! -d "$MIGRATIONS_DIR" ]]; then 
+    mkdir -p "$MIGRATIONS_DIR"
+fi
+
+for migration in ${migrations[@]}; do
+    diesel migration generate --migration-dir "$MIGRATIONS_DIR" "$migration"
+done
