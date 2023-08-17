c586f6f5e4a274c98ffa6d51e5a00d9ea32bd989
Joseph Diza <josephm.diza@gmail.com>
2023-08-17 13:20:17 -0700

    Add log statement

--- pg_start_databases.sh
+++ pg_start_databases.sh
@@ -2,6 +2,6 @@
 
 source pg_config.sh
 
-# Start our database clusters
+log "Starting database clusters"
 pg_ctl -D "$DB_CLUSTER_SHARED_DIR" -l "$DB_CLUSTER_SHARED_LOG" -o "-p $DB_CLUSTER_SHARED_PORT" start
 pg_ctl -D "$DB_CLUSTER_TARGET_DIR" -l "$DB_CLUSTER_TARGET_LOG" -o "-p $DB_CLUSTER_TARGET_PORT" start
