190f90e32e4f138523522d086272dbf86f71c6b4
Joseph Diza <josephm.diza@gmail.com>
2023-08-17 14:02:51 -0700

    Remove dead migrations

--- migrations/postgres/main/mime_types/2023-08-11-014142_create_mime_types/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-
-create TABLE mime_types (
-    id INTEGER NOT NULL PRIMARY KEY,
-    mime_type VARCHAR NOT NULL
-);
--- migrations/sqlite3/main/files/2023-07-31-235401_create_files/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-CREATE TABLE files (
-    id INTEGER NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL,
-    path TEXT NOT NULL,
-    mime_type_id INTEGER NOT NULL,
-    size INTEGER NOT NULL
-);
